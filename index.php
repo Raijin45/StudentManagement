47ecd9407f6b3f9ff8aeb6435f47965ff827dfaa
unknown <jilwinestoya2.0@gmail.com>
2025-07-25 11:51:47 +0800

    Upload sourcecode

--- index.php
+++ index.php
@@ -1,13 +1,71 @@
 <?php
 include 'vendor/autoload.php';
 
+use Docallos\StudentManagement\Core\Database;
 use Docallos\StudentManagement\Model\StudentModel;
 
-$student = new StudentModel;
-$student->id = 117006110035;
-$student->fullname = "Docallos , Jickey";
-$student->YearLevel = "First Year";
-$student->course = "BSIT";
-$student->Section = "A";
+$students = new StudentModel();
 
-$student->displayinfo();
+$students = $students->read();
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>HTML Forms </title>
+</head>
+<body>
+    <h1>Student Management</h1>
+    <form action="index.html">
+        <label for="full name">Full Name :</label><br>
+        <input type="text" id="full name" placeholder="Full Name" required><br>
+        <br>
+        <label for="year level">Year Level :</label><br>
+        <input type="text" id="year level" placeholder="Year Level" required><br>
+        <br>
+        <label for="course">Course :</label><br>
+        <input type="text" id="course" placeholder="Course" required><br>
+        <br>
+        <label for="section">Section :</label><br>
+        <input type="text" id="section" placeholder="Section" required><br>
+        <button type="create">Create</button><br>
+        <br>
+        <h2>UPDATE Student</h2>
+        <label for="id">ID :</label><br>
+        <input type="text" id="id" placeholder="ID" required><br>
+        <label for="male">Male</label><br>
+        <input type="radio" name="gender" id="female" value="Female">
+        <label for="female">Female</label><br>
+        <br>
+        <label for="status">Status :</label><br>
+        <select id="status">
+            <option value="Single">Single</option>
+            <option value="Married">Married</option>
+            <option value="Widow">Widow</option>
+        </select><br>
+        <br>
+        <label for="skills">Skills :</label><br>
+        <input type="checkbox" id="c++" value="C++" name="skills">
+        <label for="c++">C++</label><br>
+        <input type="checkbox" id="c#" value="C#" name="skills">
+        <label for="c#">C#</label><br>
+        <input type="checkbox" id="php" value="Php" name="skills">
+        <label for="php">Php</label><br>
+        <br>
+        <label for="birthday">Birthday :</label><br>
+        <input type="date" id="birthday" required><br>
+        <br>
+        <label for="age">Age</label>
+        <input type="number" min="1"><br>
+        <br>
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+    </form>
+</body>
+
+</html>
+
--- src/Model/StudentModel.php
+++ src/Model/StudentModel.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace Docallos\StudentManagement\Model;
-use Docallos\StudentManagement\Core\Crud;
 
-class StudentModel implements Crud {
+use Docallos\StudentManagement\Core\Crud;
+use Docallos\StudentManagement\Core\Database;
+
+
+class StudentModel extends Database implements Crud {
 
     public $id;
     public $fullname;
@@ -13,6 +16,7 @@ class StudentModel implements Crud {
 
     public function __construct()
     {
+        parent::__construct(); 
         $this->id ="";
         $this->fullname ="";
         $this->YearLevel ="";
@@ -22,23 +26,75 @@ class StudentModel implements Crud {
     public function displayinfo(){
         echo "id :" .$this->id."\n";
         echo "Name :" .$this->fullname."\n";
-        echo "YearLevel :" .$this->YearLevel."\n";
+        echo "YearLevel :" .$this->yearLevel."\n";
         echo "course :" .$this->course."\n";
         echo "Section :" .$this->Section."\n";
     }
 
-    public function create(){ 
-        
+    public function create() { 
+        try{
+            $sql = "INSERT  INTO students (fullname, yearlevel, course, section) VALUES (?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->blind_param("ssss", $this->fullname, $this->yearlevel, $this->course, $this->section,);
+            if($stmt->execute()){
+                return true;
+            }else {
+                return false;
+            }
+            
+        } catch (\exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
 
-    }
+    } 
+
     public function read(){
+        try {
+            $sql = "SELECT * FROM students";
+            $results = $this->conn->query($sql);
+            return $results->fetch_all(MYSQLI_ASSOC); 
+        }catch(\Exception $e){
+            echo $e->getMessage();
+        }
         
 
     }
-    public function update(){
+    public function update() {
+        try{
+            $sql = "UPDATE students SET fullname=?, yearlevel=?, course=?, section=? WHERE id=?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->blind_param("ssss", $this->fullname, $this->yearlevel, $this->course, $this->section,);
+            if($stmt->execute()){
+                return true;
+            }else {
+                return false;
+            }
+            
+        } catch (\exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+
 
     }
-    public function delete(){
+    public function delete() {
+        try{
+            $sql = "DELETE FROM students WHERE id=?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->blind_param("i", $this->id);
+            $stmt->blind_param("ssss", $this->fullname, $this->yearlevel, $this->course, $this->section,);
+            if($stmt->execute()){
+                return true;
+            }else {
+                return false;
+            }
+            
+        } catch (\exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+       
 
     }
      
